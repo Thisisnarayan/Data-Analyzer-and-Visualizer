d0ba04739abe5d4137b0ce49df09abb06f0da84f
Narayan Sharma <narayansharma1806@gmail.com>
2018-01-18 23:54:12 +0530

    database configuration files

---
+++ config/debug.php
@@ -0,0 +1,7 @@
+function debug_to_console( $data ) {
+    $output = $data;
+    if ( is_array( $output ) )
+        $output = implode( ',', $output);
+
+    echo "<script>console.log( 'Debug Objects: " . $output . "' );</script>";
+}
